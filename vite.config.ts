d9a9889c2a5ad6dd412152eb687b02b1157b20e9
blackhole1225 <147173780+nicholas951@users.noreply.github.com>
2024-08-28 08:21:02 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), nodePolyfills()],
+  plugins: [react(),nodePolyfills()],
   build: {
     outDir: 'build', 
   },
